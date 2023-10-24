b4925a77962eca4e00719f1f210cff2fc7344cb0
ShivaniPandula <143038757+ShivaniPandula@users.noreply.github.com>
2023-10-24 15:31:28 -0500

    Bonus

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var compRouter = require('./routes/computations');
+var compRouter = require('./routes/computation');
 
 var app = express();
 
